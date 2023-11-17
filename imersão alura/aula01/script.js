258f4d458d2b6b1c76c096cce07206d08e228a59
lodijoao <144748379+lodijoao@users.noreply.github.com>
2023-11-17 15:51:07 -0300

    Initial commit

---
+++ imersão alura/aula01/script.js
@@ -0,0 +1,4 @@
+var valorEmDolar = 30;
+var valorEmReal = 5.32;
+var resultado = valorEmDolar * valorEmReal
+alert('R$' + resultado.toFixed([2]))
---
+++ projeto/script.js
@@ -0,0 +1,8 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    const menuButton = document.querySelector(".btn");
+    
+    menuButton.addEventListener("click", function() {
+        alert("Menu will be displayed here.");
+    });
+});
